57832cc8c1dd88272a4ea014cb6b2b2dd6e9faa0
Damares Vitória <lima.damares@escola.pr.gov.br>
2025-06-05 14:04:48 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Fómula 1',
+    'Onde fica o GP mais conhecido da Ferrari?',
+    'Fica em Monaco'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Qual é o nome do piloto prodígio da Ferrari?',
+    'O piloto monegasco Charles Leclerc'
+)
